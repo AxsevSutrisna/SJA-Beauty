bfe3bb6650483c72c2eacb9dada770210ed4aa4c
Asep sutrisna suhada putra <98993107+AxsevSutrisna@users.noreply.github.com>
2025-07-21 10:58:06 +0700

    first commit
    percobaan commit pertama

---
+++ Back-end/Index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.get('/endpoint', (req, res) => {
+  res.send('API is working!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
